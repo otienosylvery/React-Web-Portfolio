5e1ce45c15dd0bd96f4b442f5b061d87d332599a
Rodgers Ouma <mc_alila@outlook.com>
2023-10-24 14:09:16 +0300

    #Work Experience Commit

--- client/src/components/WorkExperience/WorkExperience.js
+++ client/src/components/WorkExperience/WorkExperience.js
@@ -10,31 +10,36 @@ const WorkExperience = () => {
 
     const data =[
         {
-            companyName: "Communications Authority",
-            position: 'Full Stack Developer',
-            role: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
-            year:'2021-2022',
+            companyName: "Kenya Revenue Authority",
+            position: 'Software Developer (M-Service)',
+            role: 'As a software developer in the M-Service Section, I was invovled to a big part in designing, building, deploying and maintaining both mobile and robust system applications, developing new systems to automate business processes in conformity with design artefacts and system request chages (SRCs), collaborating with cross functional teams to analyse, design and ship new features as well as performing peer reviews and unit tests to guarantee quality before final submissions to Quality Management. Products developed include: KRA Staff App (Android) and KRA M-Service Application (Android).',
+            year:'2021-2023',
             techSkills:[
                 {
-                    techName:'Node JS'
+                    techName:'Android Studio'
+                },
+                {
+                    techName:'Java'
+                },
+                {
+                    techName:'Kotlin'
                 },
                 {
                     techName:'React JS'
                 },
                 {
-                    techName:'Postgres SQL'
+                    techName:'SpringBoot'
                 },
                 {
                     techName:'Material UI'
                 },
-
                 
             ]
         },
         {
-            companyName: "Unicool International",
-            position: 'Front End Developer',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
+            companyName: "ICT Authority of Kenya",
+            position: 'Software Developer',
+            role:'I worked as a Software Developer for the ICT Authority stationed at the Kenya Revenue Authority HQ, I took part in designing, building, deploying and maintaining system as well as web applications, developing new systems to automate business processes in conformity with design artefacts and system request chages (SRCs), collaborating with cross functional teams to analyse, design and ship new features as well as performing peer reviews and unit tests to guarantee quality before final submissions to Quality Management. Products developed include: KESRA Website, KRA Website and KESRA Mobile Application (Android).',
             year:'2020-2021',
             techSkills:[
                 {
@@ -54,111 +59,44 @@ const WorkExperience = () => {
             ]
         },
         {
-            companyName: "Kenya Revenue Authority",
-            position: 'Full Stack Developer',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
-            year:'2019-2020',
-            techSkills:[
-                {
-                    techName:'Node JS'
-                },
-                {
-                    techName:'React JS'
-                },
-                {
-                    techName:'Postgres SQL'
-                },
-                {
-                    techName:'Material UI'
-                },
-
-                
-            ]
-        },
-        {
-            companyName: "ICT Authority",
-            position: 'Full Stack Developer',
+            companyName: "Unicool International Ltd (Kenya)",
+            position: 'ICT Officer',
+            role:'As an ICT Officer at the organization, I was tasked with providing User support & helpdesk support, Installation and configuration of Operating Systems and Application Software, Setup of new computers and peripherals, User Active Directory Management, Network maintenance and troubleshooting as Unicool  website design and development.',
             year:'2018-2019',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
             techSkills:[
                 {
-                    techName:'Node JS'
+                    techName:'UAD'
                 },
                 {
-                    techName:'React JS'
+                    techName:'Computer OS'
                 },
                 {
-                    techName:'Postgres SQL'
+                    techName:'MySQL'
                 },
                 {
-                    techName:'Material UI'
+                    techName:'JavaScript'
+                },
+                {
+                    techName:'HTML/CSS'
                 },
 
                 
             ]
         },
         {
-            companyName: "Google",
-            position: 'Full Stack Developer',
-            year:'2021',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
+            companyName: "Communications Authority of Kenya",
+            position: 'ICT Student Intern',
+            role:'As an ICT Student Intern at the organization, I was tasked with providing User support & helpdesk support, Installation and configuration of Operating Systems and Application Software, Setup of new computers and peripherals, User Active Directory Management and Network maintenance and troubleshooting.',
+            year:'2016-2017',
             techSkills:[
                 {
-                    techName:'Node JS'
+                    techName:'UAD'
                 },
                 {
-                    techName:'React JS'
+                    techName:'Computer OS'
                 },
                 {
-                    techName:'Postgres SQL'
-                },
-                {
-                    techName:'Material UI'
-                },
-
-                
-            ]
-        },
-        {
-            companyName: "Microsoft",
-            position: 'Full Stack Developer',
-            year:'2021',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
-            techSkills:[
-                {
-                    techName:'Node JS'
-                },
-                {
-                    techName:'React JS'
-                },
-                {
-                    techName:'Postgres SQL'
-                },
-                {
-                    techName:'Material UI'
-                },
-
-                
-            ]
-        },
-
-        {
-            companyName: "Huawei",
-            position: 'Full Stack Developer',
-            year:'2021',
-            role:'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown ...',
-            techSkills:[
-                {
-                    techName:'Node JS'
-                },
-                {
-                    techName:'React JS'
-                },
-                {
-                    techName:'Postgres SQL'
-                },
-                {
-                    techName:'Material UI'
+                    techName:'MySQL'
                 },
 
                 
