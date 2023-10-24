bc2f9d362358cf6a279489b6780a7fb8b4ed4f71
Rodgers Ouma <mc_alila@outlook.com>
2023-10-24 15:50:53 +0300

    #Education Commit

--- client/src/components/Education/Education.js
+++ client/src/components/Education/Education.js
@@ -8,22 +8,22 @@ import { MdCastForEducation } from 'react-icons/md'
 const Education = () => {
 
     const data =[
-        {
-            institutionName: 'Jomo Kenyatta University of Agriculture & Technology',
-            qualification:'Msc. Information Technology',
-            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-            year:'2020 - Present'
-        },
+        // {
+        //     institutionName: 'Jomo Kenyatta University of Agriculture & Technology',
+        //     qualification:'Msc. Information Technology',
+        //     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+        //     year:'2020 - Present'
+        // },
         {
             institutionName: 'Jomo Kenyatta University of Agriculture & Technology',
             qualification:'Bachelor of Business Information Technology',
-            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            description:'Second Class Upper Honors Graduate',
             year: '2014 - 2018'
         },
         {
             institutionName: 'St. Joseph\'s School Rapogi',
             qualification:'KCSE Certificate',
-            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+            description:'Mean Grade: A',
             year: '2010 - 2013'
         }
     ]
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import './Home.css'
 import Contact from "../Contact/Contact"
-import { BrowserRouter,Routes, Route} from 'react-router-dom';
-import {HashLink as Link} from 'react-router-hash-link'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link'
 
 import Typewriter from 'typewriter-effect';
 import MyCv from './my_cv.pdf'
-import {BsFillMoonStarsFill, BsSun} from 'react-icons/bs'
-import {IoSunny} from 'react-icons/io5'
+import { BsFillMoonStarsFill, BsSun } from 'react-icons/bs'
+import { IoSunny } from 'react-icons/io5'
 import Fade from 'react-reveal/Fade'
 
 
 
 
 
-const Home = ({theme, changeTheme}) => {
+const Home = ({ theme, changeTheme }) => {
   return (
-    <div  className='container-fluid home' id='home'>
+    <div className='container-fluid home' id='home'>
 
       <div className='themeChange' onClick={changeTheme}>
 
-        {theme === 'lightTheme' ? (<p><BsFillMoonStarsFill size={40}/></p>):(<p className='light-them-icon'><IoSunny size={40}/></p>)}
+        {theme === 'lightTheme' ? (<p><BsFillMoonStarsFill size={40} /></p>) : (<p className='light-them-icon'><IoSunny size={40} /></p>)}
 
 
       </div>
@@ -29,51 +29,44 @@ const Home = ({theme, changeTheme}) => {
 
         <Fade right>
 
-        <h1>Hi I'm Sylvery Otieno</h1>
-        <h3>
-          <Typewriter
-            options={{
-              strings: [
-                'Full Stack Software Developer',
-                'Android Applications Developer', 
-                'UI/UX Designer',
-                'Systems Architect',
-                'Project Manager',
-                'Web Developer'
-              ],
-              autoStart: true,
-              loop: true,
-              delay:5
-            }}
-          />
-        </h3>
+          <h1>Hi I'm Sylvery Otieno</h1>
+          <h3>
+            <Typewriter
+              options={{
+                strings: [
+                  'Full Stack Software Developer',
+                  'Android Applications Developer',
+                  'UI/UX Designer',
+                  'Systems Architect',
+                  'Project Manager',
+                  'Web Developer'
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 5
+              }}
+            />
+          </h3>
 
         </Fade>
 
         <Fade bottom>
-        {/* <BrowserRouter>
-            <Routes>
-              <Route path="/contacts" element={<Contact/>}></Route>
-            </Routes>
-            </BrowserRouter> */}
-        <div className='action-button'>
+
+          <div className='action-button'>
             <BrowserRouter>
-            
-            <Link to="#contact" smooth={true} style={{textDecoration:'none', color:'black'}}><div  className='hire-me-button'>
-            Hire Me</div></Link>
-          
-            {/* <div  className='hire-me-button'>
-            Hire Me
-            <Link to="#contact" smooth={true} style={{textDecoration:'none', color:'black'}}>Hire Me</Link>
-          </div> */}
+
+              <Link to="#contact" smooth={true} duration={100} style={{ textDecoration: 'none', color: 'black' }}><div className='hire-me-button'>
+                Hire Me</div></Link>
+
+
             </BrowserRouter>
-          
 
-          <div className='get-resume-button'>
-            <a href={MyCv} download='Sylvery Otieno Resume.pdf'>Get Resume</a>
+
+            <div className='get-resume-button'>
+              <a href={MyCv} download='Sylvery Otieno Resume.pdf'>Get Resume</a>
+            </div>
+
           </div>
-
-        </div>
         </Fade>
       </div>
 
